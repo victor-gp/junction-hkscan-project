9ea4e54f87ef9396db0c366c7eb89313ffd4defe
victor-gp <victor.gp.dev@gmail.com>
2020-11-07 21:39:04 +0100

    Fix indentation in index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,10 +3,10 @@ import styles from '../components/home.module.css'
 import Link from 'next/link'
 import Layout from '../components/layout.js'
 import SlowLink from '../utils/SlowLink.js'
-import {content} from '../text-content.js'
+import { content } from '../text-content.js'
 import ReactPlayer from 'react-player'
 
-export default function Home(){
+export default function Home() {
 
   return (
     <Layout>
@@ -14,80 +14,80 @@ export default function Home(){
 
         <section className={styles.topPanel}>
 
-            {/* left */}
-            <div className={styles.intro}>
-              <div className={styles.hi}> hi! we're hkscan.ai.</div>
-              <div className={styles.blurb}>
+          {/* left */}
+          <div className={styles.intro}>
+            <div className={styles.hi}> hi! we're hkscan.ai.</div>
+            <div className={styles.blurb}>
               hkscan.AI provides a consolidated suite of data visualization and data science
-                  powered that helps
-                                  <span style={{color: "#FC3F42"}}> </span> 
- <span style={{color: "#2191FB"}}> </span>
-              </div>
+              powered that helps
+                                  <span style={{ color: "#FC3F42" }}> </span>
+              <span style={{ color: "#2191FB" }}> </span>
             </div>
+          </div>
 
-            {/* right */}
-            <div className={styles.circleContainer}>
-             <center> <img src="/images/work/HKScan_logo.png"/> 
-             </center>
-            </div>
+          {/* right */}
+          <div className={styles.circleContainer}>
+            <center> <img src="/images/work/HKScan_logo.png" />
+            </center>
+          </div>
 
         </section>
 
 
         {/*  WORK */}
-      <section className={styles.work}>
+        <section className={styles.work}>
 
-        <div className={styles.titles}>What Services Do We Provide? </div>
+          <div className={styles.titles}>What Services Do We Provide? </div>
 
-        <div className={styles.threeitemrow}>
- 
-          {/* barclays */}
-          <div className={styles.item}>
-            <SlowLink href="/ask">
-            <div className={`${styles.rectangle} ${styles.mastercardRect} ${styles.grow} ${styles.cappedSize}`}>
-                <img src="/images/work/doc.png"/>
-              </div>
-            </SlowLink>
-            <div className={styles.captions}>Intelligent Understanding</div>
-           
+          <div className={styles.threeitemrow}>
+
+            {/* barclays */}
+            <div className={styles.item}>
+              <SlowLink href="/ask">
+                <div className={`${styles.rectangle} ${styles.mastercardRect} ${styles.grow} ${styles.cappedSize}`}>
+                  <img src="/images/work/doc.png" />
+                </div>
+              </SlowLink>
+              <div className={styles.captions}>Intelligent Understanding</div>
+
+            </div>
+
+            {/* moderna */}
+            <div className={styles.item}>
+              <SlowLink href="/ask">
+                <div className={`${styles.rectangle} ${styles.mastercardRect} ${styles.grow} ${styles.cappedSize}`}>
+                  <img src="/images/work/moderna.png" />
+                </div>
+              </SlowLink>
+              <div className={styles.captions}>Custom Responses</div>
+
+            </div>
+
+            {/* mastercard */}
+            <div className={styles.item}>
+              <SlowLink href="/mastercard">
+                <div className={`${styles.rectangle} ${styles.mastercardRect} ${styles.grow} ${styles.cappedSize}`}>
+                  <img src="/images/work/chrome.png" />
+                </div>
+              </SlowLink>
+              <div className={styles.captions}>hKScan Chrome</div>
+
+            </div>
+
           </div>
 
-          {/* moderna */}
-          <div className={styles.item}>
-            <SlowLink href="/ask">
-            <div className={`${styles.rectangle} ${styles.mastercardRect} ${styles.grow} ${styles.cappedSize}`}>
-                  <img src="/images/work/moderna.png"/>
-              </div>
-            </SlowLink>
-            <div className={styles.captions}>Custom Responses</div>
-         
-          </div>
+        </section>
 
-          {/* mastercard */}
-          <div className={styles.item}>
-            <SlowLink href="/mastercard">
-              <div className={`${styles.rectangle} ${styles.mastercardRect} ${styles.grow} ${styles.cappedSize}`}>
-                  <img src="/images/work/chrome.png"/>
-              </div>
-            </SlowLink>
-            <div className={styles.captions}>hKScan Chrome</div>
-            
-          </div>
-
-        </div>
-
-      </section>
-      
-      {/* example with text-content.js */}
-      <section>
-        <div className={styles.work}>
+        {/* example with text-content.js */}
+        <section>
+          <div className={styles.work}>
             <div className={styles.titles}>{content.happy_cattle.title}</div>
             <div className={styles.blurb}>{content.happy_cattle.text1}</div>
-        </div>        
-      </section>
+          </div>
+        </section>
 
-      {/* video-embedding example */}
-      <section className={styles.work}>
+        {/* video-embedding example */}
+        <section className={styles.work}>
           <ReactPlayer
             url="https://youtu.be/UC5IqjUYRHQ"
             controls
@@ -95,12 +95,12 @@ export default function Home(){
             width="896px"
             height="504px"
           />
-      </section>      
+        </section>
 
       </main>
 
     </Layout>
 
-    )
+  )
 
 }
